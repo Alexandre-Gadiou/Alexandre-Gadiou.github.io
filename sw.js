2ee6ecc6be1d92cdaf0246a0db2b633c8b34b4e5
Alexandre Gadiou <Alex@MacBook-Pro-de-Alexandre.local>
2019-01-16 10:32:12 +0100

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-bf305351ac5c4186a1a3.js"
+    "url": "webpack-runtime-e9a4b1479947a8ed90f4.js"
   },
   {
     "url": "app-fcc16b8808ed46ef2d2e.js"
@@ -36,11 +36,11 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "abe5ebbed23577e49e972876463e3402"
+    "revision": "fe2d20a9e28f7b16497971e2e86b216f"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a39f5eac32c013d4426121f4e813adf4"
+    "revision": "11cc4e37a4e5e3e4a5cd11d20de5e4cf"
   },
   {
     "url": "1.cdc8b332050424974e92.css"
@@ -49,7 +49,7 @@ self.__precacheManifest = [
     "url": "component---src-pages-index-js-c739b973c2c164aafeb1.js"
   },
   {
-    "url": "1-5205e7c4dcd6689fa5c2.js"
+    "url": "1-47639f0721cc9689f997.js"
   },
   {
     "url": "7-f0c6e0497d243986534c.js"
