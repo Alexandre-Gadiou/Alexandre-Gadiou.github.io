6f8e1ea8472df7b7ea00ea2a6f49fd3c8445c323
Alexandre Gadiou <Alex@MBPdeAlexandre.home>
2021-06-10 15:44:54 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f83147befc918f579f41.js"
+    "url": "webpack-runtime-de560c08a9857c11bcf9.js"
   },
   {
     "url": "framework-b15755926c38e3bd7a71.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "457acaa8a88355d2356818d6c8bb5b97"
+    "revision": "3ab55dc3219ec12687ce46b4605c8bd0"
   },
   {
     "url": "polyfill-d06486d78b0d3a46b835.js"
